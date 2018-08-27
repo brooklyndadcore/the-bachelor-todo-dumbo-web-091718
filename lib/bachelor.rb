1f6d04a59b24a191737209c45c95e689dd42d1d0
scott.waring@gmail.com <scott.waring@gmail.com>
2018-08-27 18:04:34 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -4,7 +4,7 @@ def get_first_name_of_season_winner(data, season)
   arr = []
   app_sea.each do |ele|
     if ele == season
-      return ele
+      return app_sea[ele]
       # app_sea[ele].each do |ele1|
       #   puts ele1
       #   # if ele1["status"] == "Winner" 
